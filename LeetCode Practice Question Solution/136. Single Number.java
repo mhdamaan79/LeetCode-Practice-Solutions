3495be89534e2187499355ea2a3b3eac12bb1e77
Mohd Amaan <118375524+mhdamaan79@users.noreply.github.com>
2022-12-17 23:19:18 +0530

    Create 136. Single Number.java

---
+++ LeetCode Practice Question Solution/136. Single Number.java
@@ -0,0 +1,12 @@
+Difficulty - Easy
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int unique = 0;
+        for(int i=0; i<nums.length; i++) {
+            unique ^= nums[i];
+        }
+        return unique;
+    }
+}
