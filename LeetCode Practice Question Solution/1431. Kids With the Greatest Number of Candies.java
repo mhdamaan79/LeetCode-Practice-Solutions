44aeecd5416778cab538253a8687a767a0b35350
Mohd Amaan <118375524+mhdamaan79@users.noreply.github.com>
2023-01-04 17:04:50 +0530

    Update 1431. Kids With the Greatest Number of Candies.java

--- LeetCode Practice Question Solution/1431. Kids With the Greatest Number of Candies.java
+++ LeetCode Practice Question Solution/1431. Kids With the Greatest Number of Candies.java
@@ -28,3 +28,33 @@ class Solution {
         return result;
     }
 }
+
+
+
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        // Optimized Approach
+        // TC = O(2n) ~ O(n)
+        // SC = O(n)
+
+        List<Boolean> result = new ArrayList<>(candies.length);
+        int max = Integer.MIN_VALUE;
+        for( int i=0; i<candies.length; i++ ) {
+            max = Math.max( candies[i],max );        
+        }
+
+        for( int i : candies) {    // for each loop
+            result.add( i >= max - extraCandies );
+        }
+        return result;
+    }
+}
+
+
+// approach -> elements @ i + extraCandies >= greatest element in candies
+// which means element >= greatest elements - extraCandies 
+// example : [2,3,5,1,3]
+// greatest element = 5;  extraCandies = 3;
+// @ i=1,   2>=5-3 ( which means true )
+// on the otherhand @ i=3,   1!>=5-3 ( which mean false )
