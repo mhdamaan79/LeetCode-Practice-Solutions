87681288e46c4cb599e19d56fbdd9e9255b56364
Mohd Amaan <118375524+mhdamaan79@users.noreply.github.com>
2023-01-08 14:50:19 +0530

    Update 344. Reverse String.java

--- LeetCode Practice Question Solution/344. Reverse String.java
+++ LeetCode Practice Question Solution/344. Reverse String.java
@@ -1,6 +1,8 @@
 Difficulty - Easy
 
 
+// Iterative Methode
+
 class Solution {
     public void reverseString(char[] s) {
        int si = 0;
@@ -14,5 +16,31 @@ class Solution {
            si++;
            ei--;
        }
+    }
+}
+
+
+
+
+// Recursive Methode
+
+class Solution {
+    public void reverseString(char[] s) {
+       recursive( s, 0, s.length-1 );
+    }
+
+    static void recursive( char[] s, int si, int ei ) {
+        // base case
+       if( si > ei ) {
+           return;
+       }
+
+        // swapping
+       char temp = s[si];
+       s[si] = s[ei];
+       s[ei] = temp;
+
+       // recursive call
+       recursive( s, si+1, ei-1 );
     }
 }
